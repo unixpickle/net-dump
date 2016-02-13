5735152ebab553b770d29dc20933c3ec4d077ee1
Alex Nichol <unixpickle@gmail.com>
2016-02-13 11:53:33 -0500

    drafted command-line usage

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pcap.h>
+
+int main(int argc, const char ** argv) {
+  if (argc < 3) {
+    fprintf(stderr, "Usage: %s <interface> <channel> [channel ...]\n", argv[0]);
+    return 1;
+  }
+
+  char errBuff[PCAP_ERRBUF_SIZE];
+  pcap_t * pcapHandle = pcap_open_live(argv[1], 65536, 1, 100, errBuff);
+  if (pcapHandle == NULL) {
+    fprintf(stderr, "pcap error: %s\n", errBuff);
+    return 1;
+  }
+  if (pcap_set_datalink(pcapHandle, DLT_IEEE802_11_RADIO) != 0) {
+    fprintf(stderr, "pcap error: %s\n", pcap_geterr(pcapHandle));
+    pcap_close(pcapHandle);
+    return 1;
+  }
+
+  int * channels = (int *)malloc(sizeof(int)*(argc-2));
+  if (getChannelList(argc, argv, channels) < 0) {
+    fprintf(stderr, "invalid channel list.\n");
+    return 1;
+  }
+
+  free(channels);
+}
+
+int getChannelList(int argc, const char ** argv, int * channels) {
+  for (int i = 2; i < argc; i++) {
+    channels[i-2] = atoi(argv[i]);
+    if (!channels[i-2]) {
+      return -1;
+    }
+  }
+  return 0;
+}
