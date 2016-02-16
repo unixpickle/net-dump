dd79f6b0a6a0e2b464e5b41d9860985e11597d1c
Alex Nichol <unixpickle@gmail.com>
2016-02-15 21:59:56 -0500

    added response analysis to hosts analyzer
    Sometimes, the sniffer might miss a request but catch the response. A lot of the time, the response will include a cookie that gives away the hostname of the server.

--- analyzer/hosts.c
+++ analyzer/hosts.c
@@ -3,16 +3,21 @@
 #include <stdio.h>
 #include <string.h>
 
+static char * find_cookie_domain(char * headers);
+
 void hosts_command(int argc, const char ** argv, db * database) {
   string_counter * counter = string_counter_alloc();
 
   int i;
   for (i = 0; i < database->count; ++i) {
     db_entry entry = database->entries[i];
-    if (entry.request_host[0] == 0) {
-      continue;
+    if (entry.request_host[0] != 0) {
+      string_counter_add(counter, entry.request_host, 1);
     }
-    string_counter_add(counter, entry.request_host, 1);
+    char * cookieDomain = find_cookie_domain(entry.response_headers);
+    if (cookieDomain != NULL) {
+      string_counter_add(counter, cookieDomain, 1);
+    }
   }
 
   string_counter_sort(counter);
@@ -32,4 +37,25 @@ void hosts_command(int argc, const char ** argv, db * database) {
   }
 
   string_counter_free(counter);
+}
+
+static char * find_cookie_domain(char * headers) {
+  // TODO: avoid false positives by specifically making sure we are in a "Cookie" header.
+
+  ssize_t len = strlen(headers);
+  ssize_t i;
+  for (i = 0; i < len-7; ++i) {
+    if (memcmp(headers+i, "Domain=", 7) == 0) {
+      ssize_t start = i+7;
+      for (i = start; i < len; ++i) {
+        char ch = headers[i];
+        if (ch == ';' || ch == '\\') {
+          break;
+        }
+      }
+      headers[i] = 0;
+      return headers + start;
+    }
+  }
+  return NULL;
 }
