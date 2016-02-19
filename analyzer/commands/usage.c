aaa17c0d5b0313d93558a0eb949a2cda86e8430c
Alex Nichol <unixpickle@gmail.com>
2016-02-18 21:40:22 -0500

    added time boundary flags for `usage` command
    This makes it possible to analyze a specific period of time in greater detail.

--- analyzer/commands/usage.c
+++ analyzer/commands/usage.c
@@ -1,8 +1,12 @@
 #include "usage.h"
+#include "../flags.h"
 #include "../graph.h"
 
 void usage_command_help() {
-  printf("No arguments for `usage`.\n");
+  printf("Available flags:\n\n"
+    " -s <start time>  starting epoch time.\n"
+    " -e <end time>    ending epoch time.\n"
+    "\n");
 }
 
 void usage_command(int argc, const char ** argv, db * database) {
@@ -11,18 +15,32 @@ void usage_command(int argc, const char ** argv, db * database) {
     return;
   }
 
+  cmd_flags * flags = cmd_flags_parse("-s time -e time", argc, argv);
+  if (flags == NULL) {
+    return;
+  }
+
   db_sort(database);
+
+  unsigned long long startTime = cmd_flags_get_time(flags, "-s", database->entries[0].timestamp);
+  unsigned long long endTime = cmd_flags_get_time(flags, "-e",
+    database->entries[database->count-1].timestamp);
+  cmd_flags_free(flags);
 
   int bufferSize = 16;
   int valueCount = 0;
   double * values = (double *)malloc(sizeof(double) * bufferSize);
 
-  unsigned long long firstTimestamp = database->entries[0].timestamp;
-  unsigned long long currentMinute = firstTimestamp;
+  unsigned long long currentMinute = startTime;
   double currentValue = 0;
   int i;
   for (i = 0; i < database->count; ++i) {
     db_entry entry = database->entries[i];
+    if (entry.timestamp < startTime) {
+      continue;
+    } else if (entry.timestamp > endTime) {
+      continue;
+    }
     while (entry.timestamp >= currentMinute+60) {
       if (valueCount == bufferSize) {
         bufferSize *= 2;
